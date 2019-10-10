330167725509580bc6bfe6c647aef5f5d0b16d59
Williamson <wil9e9@csiro.au>
2019-10-10 11:35:23 +1000

    added code to bind three data frames into one. Started to add cols with calculated results. lunch time commit

--- scripts/data_tidy.R
+++ scripts/data_tidy.R
@@ -4,10 +4,34 @@ library(readxl) #open readxl package to allow excel files to be read in
 
 library(lubridate) #open lubridate package to allow to work with date and time data
 
+#Read data from data/comissioning_data.xlsx. This file contains three sheets each of which contain the data from a 
+#single sample type. SR0204 is a high reactivty control sample, SR0190 is a low reactivity control sample and 
+#Other are test samples that have been included in project but may not be used for assessing the effect of process
+#automation on test results.   
 
+SR0204_data<- read_excel("data/comissioning_data.xlsx",
+                         sheet="SR0204",
+                         col_names=TRUE,
+                         col_type=c("text","text","text","date","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric")) #reads data in sheet SR0204 of the file comissioning_data.xlsx, column types have been specified to facilitate binding rows.
 
-SR0204_data<- read_excel("data/comissioning_data.xlsx",sheet="SR0204",col_names=TRUE) #reads data in sheet SR0204 of the file comissioning_data.xlsx
+SR0190_data<- read_excel("data/comissioning_data.xlsx",
+                         sheet="SR0190",
+                         col_names=TRUE,
+                         col_type=c("text","text","text","date","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric")) #reads data in sheet SR0190 of the file comissioning_data.xlsx, column types have been specified to facilitate binding rows.
 
-SR0190_data<- read_excel("data/comissioning_data.xlsx",sheet="SR0190",col_names=TRUE) #reads data in sheet SR0190 of the file comissioning_data.xlsx
+other_data<- read_excel("data/comissioning_data.xlsx",
+                        sheet=3,
+                        col_names=TRUE,
+                        col_types=c("text","text","text","date","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric")) #reads data in sheet Other Samples of the file comissioning_data.xlsx, column types have been specified to facilitate binding rows.
 
-other_data<- read_excel("data/comissioning_data.xlsx",sheet=3,col_names=TRUE) #reads data in sheet Other Samples of the file comissioning_data.xlsx
+#create a single data frame and add an identifier column,. 
+
+all_data<- bind_rows(SR0204_data,SR0190_data,other_data)%>% # binds three data frames SR0204_data, SR0190_data and other_data into a single data frame and assigns the variable all_data
+  mutate(test_number= c(1:81))%>% #Add a column that gives each line a unique identifier
+  #add a column for final mass
+  #add a column for mass loss
+  #add a column for CRI
+  #add a column for CSR
+  
+
+  
