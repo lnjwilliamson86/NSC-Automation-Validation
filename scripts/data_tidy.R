0782f09ba0097e9db879bec0c966276da3d4cf11
Williamson <wil9e9@csiro.au>
2019-10-10 13:41:31 +1000

    added columns to finalise test results cols required. Commit prior to dealing with time/date data.

--- scripts/data_tidy.R
+++ scripts/data_tidy.R
@@ -24,14 +24,24 @@ other_data<- read_excel("data/comissioning_data.xlsx",
                         col_names=TRUE,
                         col_types=c("text","text","text","date","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric")) #reads data in sheet Other Samples of the file comissioning_data.xlsx, column types have been specified to facilitate binding rows.
 
-#create a single data frame and add an identifier column,. 
+#create a single data frame and add the following columns test_number,final_mass,mass_loss,CRI and CSR. 
 
 all_data<- bind_rows(SR0204_data,SR0190_data,other_data)%>% # binds three data frames SR0204_data, SR0190_data and other_data into a single data frame and assigns the variable all_data
-  mutate(test_number= c(1:81))%>% #Add a column that gives each line a unique identifier
-  #add a column for final mass
-  #add a column for mass loss
-  #add a column for CRI
-  #add a column for CSR
+  mutate(test_number= c(1:81), #Add a column that gives each line a unique identifier
+         final_mass= `Weight+10mm` + `Weight-10mm`, # calculates a new column called final_mass by adding `Weight+10mm` and `Weight-10mm`columns.
+         mass_loss=Weight_Out - final_mass, # calculates a new column called mass_loss by subracting Weight_out from final_mass
+         CRI= (((Weight_In-Weight_Out)/Weight_In)*100), #calculates a new column called CRI using formula ((Weight_In - Weight_Out)/Weight_In) *100
+         CSR= ((`Weight+10mm`/Weight_Out)*100)) #calculates a new column called CSR by using formula (`Weight+10mm`/Weight_Out)*100
+  
+  
+SR0204_data_test<- read_excel("data/comissioning_data.xlsx",
+                         sheet="SR0204",
+                         col_names=TRUE,
+                         col_type=c("text","text","text","date","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric")) #reads data in sheet SR0204 of the file comissioning_data.xlsx, column types have been specified to facilitate binding rows.
+   
+ 
+  
+  
   
 
   
