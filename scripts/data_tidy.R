56b0cdedd911246453f272ddad98e31b3424acbb
Williamson <wil9e9@csiro.au>
2019-10-10 15:15:39 +1000

    removed dates from time columns using separate and select functions. created validatoin tidy data frame. Still needs commenting

--- scripts/data_tidy.R
+++ scripts/data_tidy.R
@@ -12,17 +12,17 @@ library(lubridate) #open lubridate package to allow to work with date and time d
 SR0204_data<- read_excel("data/comissioning_data.xlsx",
                          sheet="SR0204",
                          col_names=TRUE,
-                         col_type=c("text","text","text","date","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric")) #reads data in sheet SR0204 of the file comissioning_data.xlsx, column types have been specified to facilitate binding rows.
+                         col_type=c("text","text","text","date","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","date","numeric","date","numeric","date","numeric","date","date","date")) #reads data in sheet SR0204 of the file comissioning_data.xlsx, column types have been specified to facilitate binding rows.
 
 SR0190_data<- read_excel("data/comissioning_data.xlsx",
                          sheet="SR0190",
                          col_names=TRUE,
-                         col_type=c("text","text","text","date","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric")) #reads data in sheet SR0190 of the file comissioning_data.xlsx, column types have been specified to facilitate binding rows.
+                         col_type=c("text","text","text","date","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","date","numeric","date","numeric","date","numeric","date","date","date")) #reads data in sheet SR0190 of the file comissioning_data.xlsx, column types have been specified to facilitate binding rows.
 
 other_data<- read_excel("data/comissioning_data.xlsx",
                         sheet=3,
                         col_names=TRUE,
-                        col_types=c("text","text","text","date","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric")) #reads data in sheet Other Samples of the file comissioning_data.xlsx, column types have been specified to facilitate binding rows.
+                        col_types=c("text","text","text","date","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","date","numeric","date","numeric","date","numeric","date","date","date")) #reads data in sheet Other Samples of the file comissioning_data.xlsx, column types have been specified to facilitate binding rows.
 
 #create a single data frame and add the following columns test_number,final_mass,mass_loss,CRI and CSR. 
 
@@ -32,16 +32,20 @@ all_data<- bind_rows(SR0204_data,SR0190_data,other_data)%>% # binds three data f
          mass_loss=Weight_Out - final_mass, # calculates a new column called mass_loss by subracting Weight_out from final_mass
          CRI= (((Weight_In-Weight_Out)/Weight_In)*100), #calculates a new column called CRI using formula ((Weight_In - Weight_Out)/Weight_In) *100
          CSR= ((`Weight+10mm`/Weight_Out)*100)) #calculates a new column called CSR by using formula (`Weight+10mm`/Weight_Out)*100
+
+
+#removing date information from columns. 
+
+validation<-all_data%>%
+  separate(time_to_1100oC,into= c("date","time_to_1100oC"),sep=" ")%>% #https://stackoverflow.com/questions/42996544/how-to-remove-the-date-from-a-column-containing-both-date-and-time-using-r
+  separate(Runtime_switch_CO2, into= c("date_2","Runtime_switch_CO2"),sep=" ")%>%
+  separate(runtime_max_temp_switch_CO2, into= c("date_3","runtime_max_temp_switch_CO2"),sep=" ")%>%
+  separate(runtime_min_temp_switch_CO2, into= c("date_4","runtime_min_temp_switch_CO2"),sep=" ")%>%
+  separate(time_return_range, into= c("date_5","time_return_range"),sep=" ")%>%
+  separate(total_runtime, into= c("date_6", "total_runtime"),sep= " ")%>%
+  select(-c(date,date_2,date_3,date_4,date_5,date_6))
   
-  
-SR0204_data_test<- read_excel("data/comissioning_data.xlsx",
-                         sheet="SR0204",
-                         col_names=TRUE,
-                         col_type=c("text","text","text","date","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric")) #reads data in sheet SR0204 of the file comissioning_data.xlsx, column types have been specified to facilitate binding rows.
-   
- 
-  
-  
+  #http://www.datasciencemadesimple.com/drop-variables-columns-r-using-dplyr/
   
 
   
