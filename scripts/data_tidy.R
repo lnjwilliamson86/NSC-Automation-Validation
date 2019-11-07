90d5fa43589948d41efa78cae1cfef11f62a6f12
Williamson <wil9e9@csiro.au>
2019-11-07 15:51:58 +1000

    Added in extra data and Built plots end of day commit

--- scripts/data_tidy.R
+++ scripts/data_tidy.R
@@ -2,31 +2,40 @@ library(tidyverse) #open tidyverse package
 
 library(readxl) #open readxl package to allow excel files to be read in 
 
-library(lubridate) #open lubridate package to allow to work with date and time data
+#library(lubridate) #open lubridate package to allow to work with date and time data
 
-#Read data from data/comissioning_data.xlsx. This file contains three sheets each of which contain the data from a 
-#single sample type. SR0204 is a high reactivty control sample, SR0190 is a low reactivity control sample and 
+library(tsibble)
+
+library(anytime)
+
+#Read data from data/comissioning_data.xlsx. This file contains four sheets each of which contain the data from a 
+#single sample type. SR0204 is a high reactivty control sample, SR0190 is a low reactivity control sample SR0170 is a medium reactivity control sample and 
 #Other are test samples that have been included in project but may not be used for assessing the effect of process
 #automation on test results.The results for each sample include test results as well as process data. 
 
 SR0204_data<- read_excel("data/comissioning_data.xlsx",
                          sheet="SR0204",
                          col_names=TRUE,
-                         col_type=c("text","text","text","date","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","date","numeric","date","numeric","date","numeric","date","date","date")) #reads data in sheet SR0204 of the file comissioning_data.xlsx, column types have been specified to facilitate binding rows.
+                         col_type=c("text","text","text","date","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","date","numeric","date","numeric","date","numeric","date","date","date","text")) #reads data in sheet SR0204 of the file comissioning_data.xlsx, column types have been specified to facilitate binding rows.
 
 SR0190_data<- read_excel("data/comissioning_data.xlsx",
                          sheet="SR0190",
                          col_names=TRUE,
-                         col_type=c("text","text","text","date","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","date","numeric","date","numeric","date","numeric","date","date","date")) #reads data in sheet SR0190 of the file comissioning_data.xlsx, column types have been specified to facilitate binding rows.
+                         col_type=c("text","text","text","date","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","date","numeric","date","numeric","date","numeric","date","date","date","text")) #reads data in sheet SR0190 of the file comissioning_data.xlsx, column types have been specified to facilitate binding rows.
+
+SR0170_data<- read_excel("data/comissioning_data.xlsx",
+                         sheet="SR0170",
+                         col_names=TRUE,
+                         col_type=c("text","text","text","date","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","date","numeric","date","numeric","date","numeric","date","date","date","text")) #reads data in sheet SR0170 of the file comissioning_data.xlsx, column types have been specified to facilitate binding rows.
 
 other_data<- read_excel("data/comissioning_data.xlsx",
-                        sheet=3,
+                        sheet="Other",
                         col_names=TRUE,
-                        col_types=c("text","text","text","date","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","date","numeric","date","numeric","date","numeric","date","date","date")) #reads data in sheet Other Samples of the file comissioning_data.xlsx, column types have been specified to facilitate binding rows.
+                        col_types=c("text","text","text","date","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","date","numeric","date","numeric","date","numeric","date","date","date","text")) #reads data in sheet Other Samples of the file comissioning_data.xlsx, column types have been specified to facilitate binding rows.
 
 #create a single data frame and add the following columns of test results test_number,final_mass,mass_loss,CRI and CSR. 
 
-all_data<- bind_rows(SR0204_data,SR0190_data,other_data)%>% # binds three data frames SR0204_data, SR0190_data and other_data into a single data frame and assigns the variable all_data
+all_data<- bind_rows(SR0204_data,SR0190_data,SR0170_data,other_data)%>% # binds four data frames SR0204_data, SR0190_data, SR0170 and other_data into a single data frame and assigns the variable all_data
   mutate(test_number= row_number(), #Add a column that gives each line a unique identifier
          final_mass= `Weight+10mm` + `Weight-10mm`, # calculates a new column called final_mass by adding `Weight+10mm` and `Weight-10mm`columns.
          mass_loss=Weight_Out - final_mass, # calculates a new column called mass_loss by subracting Weight_out from final_mass
@@ -39,7 +48,7 @@ all_data<- bind_rows(SR0204_data,SR0190_data,other_data)%>% # binds three data f
 #https://stackoverflow.com/questions/42996544/how-to-remove-the-date-from-a-column-containing-both-date-and-time-using-r
 #http://www.datasciencemadesimple.com/drop-variables-columns-r-using-dplyr/
 
-validation<-all_data%>% # takes the dataframe all_data and assigns to validation data frame.
+all_data_time_only<-all_data%>% # takes the dataframe all_data and assigns to validation data frame.
   separate(time_to_1100oC,into= c("date","time_to_1100oC"),sep=" ")%>% #separates col time_to_1100oC into 2 cols one called date and the other called time_to_1100oC
   separate(Runtime_switch_CO2, into= c("date_2","Runtime_switch_CO2"),sep=" ")%>% #separates col Runtime_switch_CO2 into 2 cols one called date and the other called Runtime_switch_CO2
   separate(runtime_max_temp_switch_CO2, into= c("date_3","runtime_max_temp_switch_CO2"),sep=" ")%>% #separates col runtime_max_temp_switch_CO2 into 2 cols one called date and the other called runtime_max_temp_switch_CO2
@@ -48,7 +57,16 @@ validation<-all_data%>% # takes the dataframe all_data and assigns to validation
   separate(total_runtime, into= c("date_6", "total_runtime"),sep= " ")%>% #separates col total_runtime into 2 cols one called date and the other called total_runtime
   select(-c(date,date_2,date_3,date_4,date_5,date_6))%>% # select function removes the added date-date_6 cols from the data frame. 
   view()#views the data frame
+
+#Filter all data to create a dataframe that contains just the 30 replicates that are the commissioning valaidation data and assigns to the validation_data table
+validation_data <- filter(all_data_time_only,Validation == "Y")%>%
+ view()
   
+
+
+
+
+
 #add a col to the dataframe using runtime_switch_Co2 and time_return_range to get PID_response_time using mutate. 
 #validation %>%
   #mutate(PID_response= time_return_range - Runtime_switch_CO2 )%>%
@@ -57,7 +75,7 @@ validation<-all_data%>% # takes the dataframe all_data and assigns to validation
 
 
 validation%>%
-  int_length(interval(start=time_return_range,end=Runtime_switch_CO2))%>%
+  int_length(start=time_return_range,end=Runtime_switch_CO2)%>%
   view()
 
 
@@ -67,6 +85,9 @@ int<-interval(start=hms(time_return_range),end=hms(Runtime_switch_CO2))
   
 int <- interval(start=ymd_hms("2019-01-01"),end=ymd_hms("2019-02-01"))
   
+#https://rpubs.com/joelrudinas03/422499
+#https://bookdown.org/mikemahoney218/LectureBook/working-with-dates-and-times.html
+
 int_length(int)%>%
     view()
   
--- scripts/plots.R
+++ scripts/plots.R
@@ -2,21 +2,21 @@ library(tidyverse) #open tidyverse package
 
 library(readxl) #open readxl package to allow excel files to be read in 
 
-library(lubridate) #open lubridate package to allow to work with date and time data
+#library(lubridate) #open lubridate package to allow to work with date and time data
 
 library(cowplot) #open cowplot package to create multipanel plots
 
 # create an XY plot for sample SR0204 CRI vs replicate and CSRvs replicate, colour by mode, and a reference line that shows the mean of the dataset
 #1. create a dataframe that just contains the data from sample SR0204 
-tidy_SR0204 <- validation%>% #NOTE THAT THIS WILL CHANGE WHEN THE LAST COL IS ADDED AND WILL BE CALLED using tidy_NSC_data and assign to tidy_SR0204 data frame
+tidy_SR0204 <- all_data_time_only%>% #NOTE THAT THIS WILL CHANGE WHEN THE LAST COL IS ADDED AND WILL BE CALLED using tidy_NSC_data and assign to tidy_SR0204 data frame
   group_by(Sample)%>% #group by Sample
   filter(Sample=="SR0204") #filter for sample SR0204
 
 #2. determine mean CRI & CSR for sample SR0204 ## Note that this is not needed to add the geom_hline in the XY Plots below. 
-mean_CRI <- tidy_SR0204 %>%
+mean_CRI_SR0204 <- tidy_SR0204 %>%
   summarise(mean(CRI))
 
-mean_CSR<-tidy_SR0204%>%
+mean_CSR_SR0204<-tidy_SR0204%>%
   summarise(mean(CSR))
 
 #3. create the CRI vs replicate plot and assign to SR0204_CRI_plot
@@ -45,8 +45,105 @@ ggsave("figures/SR0204_CRI_plot.png",plot=SR0204_CRI_plot)
 ggsave("figures/SR0204_CSR_plot.png",plot=SR0204_CSR_plot)
 ggsave("figures/SR0204_results_plot.png",plot = high_reactivity_results_plot)
 
+# create an XY plot for sample SR0190 CRI vs replicate and CSRvs replicate, colour by mode, and a reference line that shows the mean of the dataset
+#1. create a dataframe that just contains the data from sample SR0190 
+tidy_SR0190 <- all_data_time_only%>% #NOTE THAT THIS WILL CHANGE WHEN THE LAST COL IS ADDED AND WILL BE CALLED using tidy_NSC_data and assign to tidy_SR0204 data frame
+  group_by(Sample)%>% #group by Sample
+  filter(Sample=="SR0190") #filter for sample SR0190
 
+#2. determine mean CRI & CSR for sample SR0190 ## Note that this is not needed to add the geom_hline in the XY Plots below. 
+mean_CRI_SR0190 <- tidy_SR0190 %>%
+  summarise(mean(CRI))
 
+mean_CSR-SR190<-tidy_SR0190%>%
+  summarise(mean(CSR))
+
+#3. create the CRI vs replicate plot and assign to SR0190_CRI_plot
+SR0190_CRI_plot <-ggplot(data=tidy_SR0190, 
+                         mapping=aes(x= Replicate_Number,
+                                     y= CRI,
+                                     colour=Mode)) +
+  geom_point(shape="triangle",size=2) + 
+  geom_hline(aes(yintercept=mean(CRI)), colour = "red") +
+  labs(title= "Low Reactivity (SR0190) CRI", x="Replicate")
+
+#4. create the CSR vs replicate plot and assign to SR0190_CSR_plot
+SR0190_CSR_plot <-ggplot(data=tidy_SR0190, 
+                         mapping=aes(x= Replicate_Number,
+                                     y= CSR,
+                                     colour=Mode)) +
+  geom_point(shape="triangle",size=2) + 
+  geom_hline(aes(yintercept=mean(CSR)), colour = "red") +
+  labs(title= "Low Reactivity (SR0190) CSR", x="Replicate")
+
+#5. join the CRI and CSR plots and assign to low_reactivity_results_plot 
+low_reactivity_results_plot<-plot_grid(SR0190_CRI_plot, SR0190_CSR_plot)
+
+#6. Save all three figures to the figures folder in png format 
+ggsave("figures/SR0190_CRI_plot.png",plot=SR0190_CRI_plot)
+ggsave("figures/SR0190_CSR_plot.png",plot=SR0190_CSR_plot)
+ggsave("figures/SR0190_results_plot.png",plot = low_reactivity_results_plot)
+
+# create an XY plot for sample SR0170 CRI vs replicate and CSRvs replicate, colour by mode, and a reference line that shows the mean of the dataset
+#1. create a dataframe that just contains the data from sample SR0170 
+tidy_SR0170 <- all_data_time_only%>% #NOTE THAT THIS WILL CHANGE WHEN THE LAST COL IS ADDED AND WILL BE CALLED using tidy_NSC_data and assign to tidy_SR0204 data frame
+  group_by(Sample)%>% #group by Sample
+  filter(Sample=="SR0170") #filter for sample SR0170
+
+#2. determine mean CRI & CSR for sample SR0190 ## Note that this is not needed to add the geom_hline in the XY Plots below. 
+mean_CRI_SR0170 <- tidy_SR0170 %>%
+  summarise(mean(CRI))
+
+mean_CSR-SR170<-tidy_SR0170%>%
+  summarise(mean(CSR))
+
+#3. create the CRI vs replicate plot and assign to SR0170_CRI_plot
+SR0170_CRI_plot <-ggplot(data=tidy_SR0170, 
+                         mapping=aes(x= Replicate_Number,
+                                     y= CRI,
+                                     colour=Mode)) +
+  geom_point(shape="triangle",size=2) + 
+  geom_hline(aes(yintercept=mean(CRI)), colour = "red") +
+  labs(title= "Medium Reactivity (SR0170) CRI", x="Replicate")
+
+#4. create the CSR vs replicate plot and assign to SR0170_CSR_plot
+SR0170_CSR_plot <-ggplot(data=tidy_SR0170, 
+                         mapping=aes(x= Replicate_Number,
+                                     y= CSR,
+                                     colour=Mode)) +
+  geom_point(shape="triangle",size=2) + 
+  geom_hline(aes(yintercept=mean(CSR)), colour = "red") +
+  labs(title= "Medium Reactivity (SR0170) CSR", x="Replicate")
+
+#5. join the CRI and CSR plots and assign to medium_reactivity_results_plot 
+medium_reactivity_results_plot<-plot_grid(SR0170_CRI_plot, SR0170_CSR_plot)
+
+#6. Save all three figures to the figures folder in png format 
+ggsave("figures/SR0170_CRI_plot.png",plot=SR0170_CRI_plot)
+ggsave("figures/SR0170_CSR_plot.png",plot=SR0170_CSR_plot)
+ggsave("figures/SR0170_results_plot.png",plot = medium_reactivity_results_plot)
+
+#create a CSR vs CRI plot for the 30 replicates that are the validation tests. The geom_abline function plots the relationship from https://doi.org/10.1080/03019233.2018.1486795 CSR=100.34-1.34*CRI
+CRI_CSR_plot_validation_only <- ggplot (data=validation_data, 
+                        mapping=aes(x= CRI,
+                                    y= CSR,
+                                    colour=Sample)) +
+  geom_point(shape="triangle",size=2) + 
+  geom_abline(aes(intercept=100.34,slope=-1.34), colour = "red") +
+  labs(title= "CRI vs CSR for Automated Temperature Control Comissioning")
+  
+ggsave("figures/CRI_CSR_plot_validation_only.png",plot=CRI_CSR_plot_validation_only)
+
+#create a CSR vs CRI plot for all data shape by method. The geom_abline function plots the relationship from https://doi.org/10.1080/03019233.2018.1486795 CSR=100.34-1.34*CRI
+CRI_CSR_plot_all <- ggplot (data=all_data_time_only, 
+                        mapping=aes(x= CRI,
+                                    y= CSR,
+                                    colour=Mode)) +
+  geom_point(aes(shape=Sample),size=2) + 
+  geom_abline(aes(intercept=100.34,slope=-1.34), colour = "red") +
+  labs(title= "CRI vs CSR for Control Samples")
+
+ggsave("figures/CRI_CSR_plot_all.png",plot=CRI_CSR_plot_all)
 
 
 
