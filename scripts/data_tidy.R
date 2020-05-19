d5639751a282ec6c660807ef15ef849539d952fc
Williamson <wil9e9@csiro.au>
2020-05-19 13:33:45 +1000

    removed code to add other data tot the data setadded code to determine means

--- scripts/data_tidy.R
+++ scripts/data_tidy.R
@@ -10,7 +10,7 @@ library(anytime)
 
 #Read data from data/comissioning_data.xlsx. This file contains four sheets each of which contain the data from a 
 #single sample type. SR0204 is a high reactivty control sample, SR0190 is a low reactivity control sample SR0170 is a medium reactivity control sample and 
-#Other are test samples that have been included in project but may not be used for assessing the effect of process
+#Other are test samples that have been included in project but may not be used for assessing the effect of process, this has been commented out as the othertab isn't working properly. 
 #automation on test results.The results for each sample include test results as well as process data. 
 
 SR0204_data<- read_excel("data/comissioning_data.xlsx",
@@ -28,25 +28,62 @@ SR0170_data<- read_excel("data/comissioning_data.xlsx",
                          col_names=TRUE,
                          col_type=c("text","text","text","date","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","date","numeric","date","numeric","date","numeric","date","date","date","text")) #reads data in sheet SR0170 of the file comissioning_data.xlsx, column types have been specified to facilitate binding rows.
 
-other_data<- read_excel("data/comissioning_data.xlsx",
-                        sheet="Other",
-                        col_names=TRUE,
-                        col_types=c("text","text","text","date","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","date","numeric","date","numeric","date","numeric","date","date","date","text")) #reads data in sheet Other Samples of the file comissioning_data.xlsx, column types have been specified to facilitate binding rows.
+#other_data<- read_excel("data/comissioning_data.xlsx",
+                       #sheet="Other",
+                        #col_names=TRUE,
+                        #col_types=c("text","text","text","date","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","date","numeric","date","numeric","date","numeric","date","date","date","text")) #reads data in sheet Other Samples of the file comissioning_data.xlsx, column types have been specified to facilitate binding rows.
 
 #create a single data frame and add the following columns of test results test_number,final_mass,mass_loss,CRI and CSR. 
 
-all_data<- bind_rows(SR0204_data,SR0190_data,SR0170_data,other_data)%>% # binds four data frames SR0204_data, SR0190_data, SR0170 and other_data into a single data frame and assigns the variable all_data
+all_data<- bind_rows(SR0204_data,SR0190_data,SR0170_data)%>% # binds four data frames SR0204_data, SR0190_data, SR0170_data into a single data frame and assigns the variable all_data
   mutate(test_number= row_number(), #Add a column that gives each line a unique identifier
          final_mass= `Weight+10mm` + `Weight-10mm`, # calculates a new column called final_mass by adding `Weight+10mm` and `Weight-10mm`columns.
          mass_loss=Weight_Out - final_mass, # calculates a new column called mass_loss by subracting Weight_out from final_mass
          CRI= (((Weight_In-Weight_Out)/Weight_In)*100), #calculates a new column called CRI using formula ((Weight_In - Weight_Out)/Weight_In) *100
          CSR= ((`Weight+10mm`/Weight_Out)*100), #calculates a new column called CSR by using formula (`Weight+10mm`/Weight_Out)*100
          PID_response= (time_return_range - Runtime_switch_CO2))%>%
-   view()#views the dataframe
+   #view()#views the dataframe
 
 #Filter all data to create a dataframe that contains just the 30 replicates that are the commissioning valaidation data and assigns to the validation_data table
-validation_data <- filter(all_data_time_only,Validation == "Y")%>%
-  view()
+validation_data <- filter(all_data,Validation == "Y")%>%
+  #view()#views the data frame
+
+#Save all_data drate frame as CSV in results folder
+write_csv(all_data,"results/all_data_output.csv",na = "NA", append = FALSE, col_names = TRUE )
+
+#Save validation_data data frame as CSV in results folder
+write_csv(validation_data,"results/validation_data_output.csv",na = "NA", append = FALSE, col_names = TRUE )
+
+#Determine mean CSR for each sample and assigns to mean_CSR
+mean_CSR<-all_data%>%
+  group_by(Sample)%>%
+  summarise(mean= mean(CSR), n=n())
+
+#Determine mean CRI for each sample and assigns to mean_CRI
+mean_CRI<-all_data%>%
+  group_by(Sample)%>%
+  summarise(mean= mean(CRI), n=n())
+
+#Determine mean CSR for autoamted runs for each sample and assigns to mean_auto_CSR
+mean_auto_CSR<-all_data%>%
+  filter(Mode == "Auto")%>%
+  group_by(Sample)%>%
+  summarise(mean= mean(CSR), n=n())
+
+#Determine mean CRI for automated runs for each sample and assigns to mean_auto_CRI
+mean_auto_CRI<-all_data%>%
+  filter(Mode == "Auto")%>%
+  group_by(Sample)%>%
+  summarise(mean= mean(CRI), n=n())
+
+
+
+
+
+
+
+
+
 
 
 
